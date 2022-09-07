4fb854978df28d379820a68dbe266525ca93251b
DongSoong <99469599+DongSoong2@users.noreply.github.com>
2022-09-07 13:12:14 +0900

    qerqrqr

--- src/main/java/sample/controller/FileController.java
+++ src/main/java/sample/controller/FileController.java
@@ -96,7 +96,7 @@ public class FileController {
 	@RequestMapping(value = "/uploadFiles.do")
 	public NexacroResult uploadFiles(MultipartFile image, String dirName) throws Exception {
 
-		logger.debug("---------------------- nexacro platform uploadFiles ---------------------------");
+		logger.debug("----------fff------------ nexacro platform uploadFiles ---------------------------");
 
 		// 반환될 파일저장 정보 Dataset 생성
 		DataSet resultDs = createDataSet4UploadResult();
